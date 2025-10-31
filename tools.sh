ced8b168c96c071b9b878d0017540ee2a94fc3de
tirucloud <137248129+tirucloud@users.noreply.github.com>
2025-10-31 08:16:23 +0530

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -38,3 +38,6 @@ curl -fsSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo gpg --dea
 echo "deb [signed-by=/usr/share/keyrings/nodesource-archive-keyring.gpg] https://deb.nodesource.com/node_16.x focal main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 sudo apt update
 sudo apt install -y nodejs
+
+# Install maven
+sudo apt install maven -y
