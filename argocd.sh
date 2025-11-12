737d028587f1b224bb98652f3e488bc2e66d0194
tirucloud <137248129+tirucloud@users.noreply.github.com>
2025-11-12 15:41:38 +0530

    Create argocd.sh

---
+++ argocd.sh
@@ -0,0 +1,11 @@
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+
+sudo apt install jq -y
+
+export ARGOCD_SERVER=`kubectl get svc argocd-server -n argocd -o json | jq --raw-output '.status.loadBalancer.ingress[0].hostname'`
+export ARGO_PWD=`kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d`
+echo $ARGOCD_SERVER
+echo $ARGO_PWD
