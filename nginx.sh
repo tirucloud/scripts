ca8ec354e06dd7d40a0b0961e7e09ba7e77a6380
tirucloud <137248129+tirucloud@users.noreply.github.com>
2025-12-13 14:12:30 +0530

    Create nginx.sh

---
+++ nginx.sh
@@ -0,0 +1,9 @@
+```
+sudo wget -O /tmp/lugx_temp.zip https://templatemo.com/download/templatemo_589_lugx_gaming
+sudo unzip -q /tmp/lugx_temp.zip -d /var/www/
+sudo mv /var/www/templatemo_589_lugx_gaming/* /var/www/html/
+sudo rm -rf /tmp/lugx_temp.zip /var/www/templatemo_589_lugx_gaming
+sudo chown -R www-data:www-data /var/www/html
+sudo chmod -R 755 /var/www/html
+sudo systemctl restart nginx
+```
